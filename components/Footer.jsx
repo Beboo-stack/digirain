f5cc0b80a9f15f1c761202a98f07fa08b000a48c
Beboo-stack <peteralber735@gmail.com>
2024-05-25 15:01:52 +0300

    z

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,21 +4,20 @@ const Footer = () => {
   return (
     <section>
       <div className="bg-[#FFCE26] text-black">
-        <div className="w-full h-full px-4 py-8 sm:px-6 sm:py-14 lg:px-32 lg:py-20 flex flex-col md:flex-row justify-between items-center">
+        <div className="w-full h-full px-4 py-8 sm:px-6 sm:py-14 lg:px-32 lg:py-24 flex flex-col md:flex-row justify-between items-center">
           {/* Text */}
           <div className="flex flex-1 flex-col gap-5">
             <h2 className="text-5xl font-bold">
-              Unlock New Horizons with our Solutions
+              Discover How We Can Help Your Business Grow
             </h2>
             <p>
-              Ready to elevate your experience? Embrace the extraordinary with
-              Dottopia. Dive in now and let’s create something remarkable
-              together!
+              Get in touch with our team of digital marketing experts to help
+              you reach your business goals.
             </p>
           </div>
           <div className="flex-1 flex justify-start md:justify-end items-center w-full h-full">
-            <p className=" font-bold py-4 px-5 bg-white w-fit rounded-lg mt-6">
-              Book a Meeting
+            <p className="capitalize font-bold py-4 px-5 bg-white w-fit rounded-lg mt-6">
+              get a proposal
             </p>
           </div>
         </div>
