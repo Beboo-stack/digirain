deb38c520fe9226e28cf861f5a6f4ce2e33aef66
Beboo-stack <peteralber735@gmail.com>
2024-05-27 01:07:09 +0300

    1

--- app/who-we-are/page.jsx
+++ app/who-we-are/page.jsx
@@ -1,3 +1,4 @@
+import CoreValues from "@/components/CoreValues";
 import Insights from "@/components/Insights";
 import Image from "next/image";
 import React from "react";
@@ -5,7 +6,8 @@ import React from "react";
 const page = () => {
   return (
     <section className="mb-48  w-full h-full">
-      <div className="w-full flex flex-col-reverse md:flex-row bg-yellow justify-center items-center h-full md:h-screen px-4 py-8 sm:px-6 sm:py-12 lg:px-32 lg:py-36 ">
+      {/* Hero Section */}
+      <div className="w-full flex flex-col-reverse md:flex-row bg-yellow justify-center items-center h-full md:h-screen px-4 py-8 sm:px-6 sm:py-12 lg:px-32 lg:py-36 mb-20">
         {/* Text */}
         <div className=" h-full flex-1 flex-col justify-center items-center py-16">
           <h2 className="text-5xl font-bold leading-snug">
@@ -30,6 +32,7 @@ const page = () => {
         </div>
       </div>
       <Insights />
+      <CoreValues />
     </section>
   );
 };
---
+++ components/CoreValues.jsx
@@ -0,0 +1,90 @@
+import Image from "next/image";
+import React from "react";
+
+const CoreValues = () => {
+  const CoreValues = [
+    {
+      id: 1,
+      title: "Work Smart Play Hard",
+      description:
+        "Always strive for efficient productivity. Then, enjoy parties and fun!",
+      svg: "/1.svg",
+    },
+    {
+      id: 2,
+      title: "Make it Happen",
+      description:
+        "Success demands dedication and time management to achieve our goals.",
+      svg: "/meeting.svg",
+    },
+    {
+      id: 3,
+      title: "Make it Happen",
+      description:
+        "Success demands dedication and time management to achieve our goals.",
+      svg: "/3.svg",
+    },
+    {
+      id: 4,
+      title: "Make your customer happy",
+      description:
+        "Customers are valued partners in achieving remarkable success, which brings us joy.",
+      svg: "/meeting.svg",
+    },
+    {
+      id: 4,
+      title: "Think Like an Owner",
+      description:
+        "Customers are valued partners in achieving remarkable success, which brings us joy.",
+      svg: "/1.svg",
+    },
+  ];
+
+  return (
+    <section className=" text-black mt-12">
+      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+        <div className="mx-auto max-w-5xl text-center mb-24">
+          <div className="">
+            <h2 className="text-center text-yellow font-bold">CORE VALUES</h2>
+            <h2 className="text-3xl font-bold sm:text-5xl">
+              What makes us different
+            </h2>
+          </div>
+
+          <p className="mt-4 w-3/4 mx-auto">
+            Our values shape the way we work with our clients, delivering
+            outstanding user experience supported by cutting-edge technologies
+          </p>
+        </div>
+
+        <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+          {CoreValues.map((item) => (
+            <div
+              className=" flex rounded-xl  space-x-3 bg-[#FFF5D1]  py-5 px-3 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+              key={item.id}
+            >
+              <div className="w-full h-[200px] bg-[#FFCE26] rounded-xl flex-1 flex justify-center items-center">
+                <Image
+                  src={item.svg}
+                  className="w-full"
+                  alt={item.title}
+                  width={50}
+                  height={50}
+                />
+              </div>
+
+              <div className="w-full h-ful1 flex-1 flex flex-col justify-center items-start">
+                <h2 className="mt-4 text-base font-bold text-black">
+                  {item.title}
+                </h2>
+                <p className="mt-3 text-sm ">{item.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CoreValues;
