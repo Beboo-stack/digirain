4a9c8708f363f81e4e4a959f2bb621d3d35d1583
Beboo-stack <peteralber735@gmail.com>
2024-05-27 13:31:25 +0300

    s

--- app/who-we-are/page.jsx
+++ app/who-we-are/page.jsx
@@ -8,7 +8,7 @@ const page = () => {
   return (
     <section className="mb-48  w-full h-full">
       {/* Hero Section */}
-      <div className="w-full flex flex-col-reverse md:flex-row bg-yellow justify-center items-center h-full md:h-screen px-4 py-8 sm:px-6 sm:py-12 lg:px-32 lg:py-36 mb-20">
+      <div className="w-full flex flex-col-reverse lg:flex-row bg-yellow justify-center items-center h-full md:h-screen px-4 py-8 sm:px-6 sm:py-12 lg:px-32 lg:py-36 mb-20">
         {/* Text */}
         <div className=" h-full flex-1 flex-col justify-center items-center py-16">
           <h2 className="text-5xl font-bold leading-snug">
